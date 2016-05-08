2e6e421e58d04cd0e0da5273a55fb2910216fcf0
Ralph Bodenner <rkbodenner+github@gmail.com>
2016-05-07 20:46:14 -0700

    Found some decent initial weights

--- nnAdd.m
+++ nnAdd.m
@@ -12,9 +12,28 @@ input_layer_size = 2;
 hidden_layer_size = 3;
 output_layer_size = 1;
 
-% Weights
-Theta1 = randInitWeights(input_layer_size, hidden_layer_size);
-Theta2 = randInitWeights(hidden_layer_size, output_layer_size);
+% Weights. These seem good.
+Theta1 = [
+  0.101237  -0.045077 -0.069981 ;
+  0.030126 -0.071757 -0.116876 ;
+  0.004693 -0.044443 0.007245
+];
+
+Theta2 = [
+  0.032645 -0.061070 0.064763 -0.066527
+];
+
+% or randomly...
+%Theta1 = randInitWeights(input_layer_size, hidden_layer_size);
+%Theta2 = randInitWeights(hidden_layer_size, output_layer_size);
+
+fprintf("Initial weights\n");
+fprintf("Theta1\n");
+fprintf("%f\n", Theta1);
+fprintf("\nTheta2\n");
+fprintf("%f\n", Theta2);
+fprintf("\n");
+
 % Regularization factor
 lambda = 0;
 
